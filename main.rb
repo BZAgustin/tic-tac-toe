ef998c57dea1926c33682681bb50f56ef270c6e2
BZAgustin <abustoszanelli@gmail.com>
2022-05-10 13:25:01 -0300

    - Specify added players' names - Add end method call

--- main.rb
+++ main.rb
@@ -5,9 +5,11 @@ require './player'
 
 # Create new game object
 game = Game.new
-player_one = Player.new
-player_two = Player.new
+player_one = Player.new('Player 1')
+player_two = Player.new('Player 2')
 
 game.start(player_one, player_two)
 
 game.begin_turn(player_one, player_two)
+
+game.end(player_one, player_two)
